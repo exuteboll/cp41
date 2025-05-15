b6edca41f4b1107c4a007f940b1d68fa6423f509
exuteboll <exuteboll4@gmail.com>
2025-05-15 10:20:49 +0300

    Add files via upload

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class Program
+
+    {
+        public delegate double Delegatechis(double x, double y);
+        public static double Min(double a, double b)
+        {
+            return a < b ? a : b;
+        }
+        public static double Max(double a, double b)
+        {
+            return a > b ? a : b;
+        }
+        static void Main(string[] args)
+        {
+            double a = 19.5;
+            double b = 15.2;
+            Delegatechis delegmin = Min;
+            Delegatechis delegmax = Max;
+
+            Console.WriteLine($"Минимальное {delegmin(a,b)}");
+            Console.WriteLine($"Максимальное {delegmax(a,b)}");
+
+            Console.ReadKey();
+        }
+    }
+}
